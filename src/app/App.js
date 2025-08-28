32aa0e3af1e3e8b11870a6f974892389ec96c76a
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-08-28 14:04:05 +0300

    added page registration

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,25 @@
+import Handlebars from 'handlebars';
+// import * as Pages from '../pages';
+
+import Button from '../components/atoms/button';
+import  { Auth }  from '../pages/auth';
+import { Registration } from '../pages/registration'
+
+Handlebars.registerPartial('Button', Button);
+Handlebars.registerPartial('Auth', Auth);
+Handlebars.registerPartial('Registration', Registration);
+
 export default class App{
-    constructor(){
-        console.log('app include');
+
+    render(){
+        const app = document.getElementById('app');
+        let pages = 'Auth';
+
+        let template = `<div>
+            {{> ${pages}}}
+        </div>`
+        const appTemplate = Handlebars.compile(template);
+        app.innerHTML = appTemplate();
     }
     
 }
-
