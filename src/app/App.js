c2aae6a248bdb6accbc66186acb9bcc8f372ba3c
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-08-29 09:42:37 +0300

    added link for pages

--- src/app/App.js
+++ src/app/App.js
@@ -1,29 +1,48 @@
-import Handlebars from 'handlebars';
-// import * as Pages from '../pages';
+import  Handlebars  from 'handlebars';
 
-import Button from '../components/atoms/button';
-import  { Auth }  from '../pages/auth';
-import { Registration } from '../pages/registration'
-import { Navigate } from '../components/organism/navigate';
+// import components
+import  { Button } from '../components/atoms/button'
+import  { Navigate } from '../components/organism/navigate';
 
+//import templates
+
+//import pages
+import  { Auth }            from '../pages/auth';
+import  { Registration }    from '../pages/registration'
+import  { UserSettings }    from '../pages/userSettings';
+import  { Chats }           from '../pages/chats'
+import  { Page404 }         from '../pages/404';
+import  { Page500 }         from '../pages/500';
+
+//registerPartial components
 Handlebars.registerPartial('Button', Button);
+Handlebars.registerPartial('Navigate', Navigate);
+
+//registerPartial templates
+
+//registerPartial pages
 Handlebars.registerPartial('Auth', Auth);
 Handlebars.registerPartial('Registration', Registration);
-Handlebars.registerPartial('Navigate', Navigate);
+Handlebars.registerPartial('UserSettings', UserSettings);
+Handlebars.registerPartial('Chats', Chats);
+Handlebars.registerPartial('Page404', Page404);
+Handlebars.registerPartial('Page500', Page500);
+
 
 export default class App{
     constructor(){
-        this.pages = {template:'Navigate'};
+        this.pages = {template:'Auth'};
     }
     
     render(){
         const app = document.getElementById('app');
         console.log(this.pages.template)
 
-        let template = `<div>
-            {{> Auth}}
-            {{> ${this.pages.template}}}
-        </div>`
+        let template = 
+            `<div>
+                {{> ${this.pages.template}}}
+                {{> Navigate}}
+            </div>`;
         const appTemplate = Handlebars.compile(template);
         app.innerHTML = appTemplate();
 
