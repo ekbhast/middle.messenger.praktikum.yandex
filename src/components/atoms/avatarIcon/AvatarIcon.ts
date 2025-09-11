c06d4ddc297004e1163827bd96ba95d912b99bbe
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-09-11 14:13:27 +0300

    added block AvatarIcon

---
+++ src/components/atoms/avatarIcon/AvatarIcon.ts
@@ -0,0 +1,21 @@
+import Block from "../../../framework/Block";
+
+interface AvatarIconProps {
+  class?: string;
+  imgSrc?: string;
+  attr?: Record<string, string>;
+}
+
+export default class AvatarIcon extends Block {
+  constructor(props: AvatarIconProps) {
+    super(props);
+  }
+
+  protected render(): string {
+    return `
+        <div class="{{class}}">
+            <img src="{{imgSrc}}" class="avatarIcon__img" />
+        </div>
+        `;
+  }
+}
--- src/framework/Block.ts
+++ src/framework/Block.ts
@@ -218,4 +218,6 @@ export default class Block {
       }
     });
   }
+
+   
 }
