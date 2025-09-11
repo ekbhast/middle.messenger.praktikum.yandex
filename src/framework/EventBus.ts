55650663a6fbdf5cacfe29535e7975130d812bd9
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-09-11 12:32:39 +0300

    added block

--- src/framework/EventBus.ts
+++ src/framework/EventBus.ts
@@ -1,31 +1,30 @@
-export type EventCallback = (...args: any[]) => void;
+export type EventCallback = (...args: unknown[]) => void;
 
-export default class EventBus{
-    private listeners: Record<string, EventCallback[]>
-    constructor(){
-        this.listeners = {}
+export default class EventBus {
+  private listeners: Record<string, EventCallback[]>;
+  constructor() {
+    this.listeners = {};
+  }
+  public on(event: string, callback: EventCallback): void {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
     }
-    public on(event: string, callback: EventCallback): void{
-        if (!this.listeners[event]){
-            this.listeners[event] = [];
-        }
-        this.listeners[event].push(callback);
-    };
-    public off(event: string, callback: EventCallback): void{
-        if (!this.listeners[event]){
-            throw new Error(`No event ${event}`);
-        }
-        this.listeners[event] = this.listeners[event].filter(
-            listeners => listeners !== callback
-        )
-    };
-
-    public emit(event: string, ...args: any[]): void{
-        if (!this.listeners[event]){
-            throw new Error(`No event ${event}`);
-        }
-        this.listeners[event].forEach((listener)=>{
-            listener(...args);
-        })
-    };
+    this.listeners[event].push(callback);
+  }
+  public off(event: string, callback: EventCallback): void {
+    if (!this.listeners[event]) {
+      throw new Error(`No event ${event}`);
+    }
+    this.listeners[event] = this.listeners[event].filter(
+      (listeners) => listeners !== callback
+    );
+  }
+  public emit(event: string, ...args: any[]): void {
+    if (!this.listeners[event]) {
+      throw new Error(`No event ${event}`);
+    }
+    this.listeners[event].forEach((listener) => {
+      listener(...args);
+    });
+  }
 }
