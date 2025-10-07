190023258beb9dd86cc240605582af234bac05c3
Aleksey Kirillov <alexey.kirillov@maergroup.ru>
2025-10-07 17:06:00 +0300

    clear dom router

--- src/framework/Router.ts
+++ src/framework/Router.ts
@@ -37,6 +37,8 @@ export default class Router {
 
         if (this._currentRoute && this._currentRoute !== route) {
             this._currentRoute.leave();
+            // полностью очищаем контейнер перед рендером нового компонента
+            this._rootQuery.innerHTML = '';
         }
 
         this._currentRoute = route;
